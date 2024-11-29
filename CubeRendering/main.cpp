44a2223ad6a4596d5bac794aedfe57537bd026fd
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-11-29 08:51:02 +0100

    Created own branch, added cube and keyboard inputs

--- CubeRendering/main.cpp
+++ CubeRendering/main.cpp
@@ -2,34 +2,48 @@
 #include <GLFW\glfw3.h>
 #include <iostream>
 
+using namespace std;
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+void keyCallback( GLFWwindow* window, int key, int scancode, int action, int mods );
+void DrawCube( GLfloat centerPosX, GLfloat centerPosY, GLfloat centerPosZ, GLfloat edgeLength );
+
+GLfloat rotationX = 0.0f;
+GLfloat rotationY = 0.0f;
 
 int main() {
 	//Inittialize GLFW
 	glfwInit();
 
-
 	//window specs / create window object
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Cube Render", NULL, NULL);
-	if (window == NULL) {
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Cube Render", NULL, NULL);
+
+
+	glfwSetKeyCallback(window, keyCallback);
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
+
+	int screenWidth;
+	int screenHeight;
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+
+	if (!window) {
 		std::cout << "Faild to create Window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
 	//introduce window into the current context
 	glfwMakeContextCurrent(window);
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1000);
+	glMatrixMode(GL_MODELVIEW);
 
-
-
-	//Square endpoints
-	float verticies[] = {
-		//x   y    z
-		-0.5f, 0.5f, 0.0f, //top left corner
-		0.5f, 0.5f, 0.0f, //top right corner
-		0.5f, -0.5f, 0.0f, //bottom right corner
-		-0.5f, -0.5f, 0.0f // bottom left corner
-	};
-
+	GLfloat halfScreenWidth = SCREEN_WIDTH / 2;
+	GLfloat halfScreenHeight = SCREEN_HEIGHT / 2;
 
 	//run window
 	while (!glfwWindowShouldClose(window)) {
@@ -38,21 +52,21 @@ int main() {
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//Render square
-		glEnableClientState(GL_VERTEX_ARRAY);
+		//Render OpenGL here:
+		glPushMatrix();
+		glTranslatef(halfScreenWidth, halfScreenHeight, -500);
+		glRotatef(rotationX, 1, 0, 0);
+		glRotatef(rotationY, 0, 1, 0);
+		glTranslatef(-halfScreenWidth, -halfScreenHeight, 500);
 
-		// (xyz=3, datatype, stride/gap?, array) 
-		glVertexPointer(3, GL_FLOAT, 0, verticies);
+		DrawCube(halfScreenWidth, halfScreenHeight, -500, 200);
 
-		//(Type of shape, startingpoint, numer of points)
-		glDrawArrays(GL_QUADS, 0, 4);
-
-		glDisableClientState(GL_VERTEX_ARRAY);
-
+		glPopMatrix();
 		//Swap front and back buffers
 		glfwSwapBuffers(window);
 
 		// Poll for and proccess events
+		// Detects different proccesses and events
 		glfwPollEvents();
 	}
 
@@ -62,3 +76,81 @@ int main() {
 
 	return 0;
 }
+
+
+void DrawCube(GLfloat centerPosX, GLfloat centerPosY, GLfloat centerPosZ, GLfloat edgeLength) {
+	GLfloat halfSideLength = edgeLength * 0.5;
+
+	GLfloat verticies[] =
+	{
+		//front face
+		centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, //top left
+		centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, //top right
+		centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, //bottom right
+		centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, //bottom left
+
+		//back face
+		centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, //top left
+		centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, //top right
+		centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, //bottom right
+		centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, //bottom left
+
+		//left face
+		centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, //top left
+		centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, //top right
+		centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, //bottom right
+		centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, //bottom left
+
+		//right face
+		centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, //top left
+		centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, //top right
+		centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, //bottom right
+		centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, //bottom left
+
+		//top face
+		centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, //top left
+		centerPosX - halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, //top right
+		centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ - halfSideLength, //bottom right
+		centerPosX + halfSideLength, centerPosY + halfSideLength, centerPosZ + halfSideLength, //bottom left
+
+		//bottom face
+		centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, //top left
+		centerPosX - halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, //top right
+		centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ - halfSideLength, //bottom right
+		centerPosX + halfSideLength, centerPosY - halfSideLength, centerPosZ + halfSideLength, //bottom left
+	};
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//Render squares
+	glEnableClientState(GL_VERTEX_ARRAY);
+
+	// (xyz=3, datatype, stride/gap?, array) 
+	glVertexPointer(3, GL_FLOAT, 0, verticies);
+
+	//(Type of shape, startingpoint, numer of points)
+	glDrawArrays(GL_QUADS, 0, 24);
+
+	glDisableClientState(GL_VERTEX_ARRAY);
+};
+
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	const GLfloat rotationSpeed = 10;
+
+	if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+		switch (key) {
+		case GLFW_KEY_UP:
+			rotationX -= rotationSpeed;
+			break;
+		case GLFW_KEY_DOWN:
+			rotationX += rotationSpeed;
+			break;
+		case GLFW_KEY_RIGHT:
+			rotationY += rotationSpeed;
+			break;
+		case GLFW_KEY_LEFT:
+			rotationY -= rotationSpeed;
+			break;
+		}
+
+	}
+};
