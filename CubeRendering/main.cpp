edc05be485a20b1c231081ec237a08f26c2bd84c
Robin Alexander Kaastrup <robin.kaastrup@no.experis.com>
2024-12-03 10:21:24 +0100

    Created branch to do some testing, and get Modern OpenGL working

--- CubeRendering/main.cpp
+++ CubeRendering/main.cpp
@@ -1,64 +1,44 @@
-#include <GL\glew.h>
-#include <GLFW\glfw3.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+int main(void)
+{
+    GLFWwindow* window;
 
-int main() {
-	//Inittialize GLFW
-	glfwInit();
-
-
-	//window specs / create window object
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Cube Render", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Faild to create Window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	//introduce window into the current context
-	glfwMakeContextCurrent(window);
-	glViewport(0, 0, 800, 800);
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
 
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-	//Square endpoints
-	float verticies[] = {
-		//x   y    z
-		-0.5f, 0.5f, 0.0f, //top left corner
-		0.5f, 0.5f, 0.0f, //top right corner
-		0.5f, -0.5f, 0.0f, //bottom right corner
-		-0.5f, -0.5f, 0.0f // bottom left corner
-	};
+    if (glewInit() != GLEW_OK)
+        std::cout << "ERROR!" << std::endl;
 
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
-	//run window
-	while (!glfwWindowShouldClose(window)) {
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		//Change color of the window
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
 
-		//Render square
-		glEnableClientState(GL_VERTEX_ARRAY);
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
 
-		// (xyz=3, datatype, stride/gap?, array) 
-		glVertexPointer(3, GL_FLOAT, 0, verticies);
-
-		//(Type of shape, startingpoint, numer of points)
-		glDrawArrays(GL_QUADS, 0, 4);
-
-		glDisableClientState(GL_VERTEX_ARRAY);
-
-		//Swap front and back buffers
-		glfwSwapBuffers(window);
-
-		// Poll for and proccess events
-		glfwPollEvents();
-	}
-
-	//terminate window on exit
-	glfwDestroyWindow(window);
-	glfwTerminate();
-
-	return 0;
+    glfwTerminate();
+    return 0;
 }
