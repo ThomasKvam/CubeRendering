0321bdd8fbf49e40e1682c2a82348e50c169ad6b
Robin Alexander Kaastrup <robin.kaastrup@no.experis.com>
2024-12-11 12:54:39 +0100

    Implemented Renderer abstraction

--- CubeRendering/main.cpp
+++ CubeRendering/main.cpp
@@ -9,6 +9,7 @@
 #include "Renderer.h"
 
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
@@ -70,6 +71,8 @@ int main(void)
         ib.Unbind();
         shader.Unbind();
 
+        Renderer renderer
+            ;
         float r = 0.0f;
         float increment = 0.01f;
 
@@ -77,15 +80,14 @@ int main(void)
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            GLCall(glClear(GL_COLOR_BUFFER_BIT));
+            renderer.Clear();
 
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-            va.Bind();
-            ib.Bind();
+
+            renderer.Draw(va, ib, shader);
 
 
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
             if (r > 1.0f)
                 increment = -0.01f;
