17c0d693deb72704aae446c038cdbab5c0d7406a
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-12-11 12:34:39 +0100

    Added textures and texture binding. Changed the shader to implement the texture

--- CubeRendering/Shader.h
+++ CubeRendering/Shader.h
@@ -14,7 +14,7 @@ class Shader {
 private:
 	std::string m_Filepath;
 	unsigned int m_RendererID;
-	std::unordered_map<std::string, unsigned int> m_UniformLocationChache;
+	std::unordered_map<std::string, int> m_UniformLocationChache;
 public:
 	Shader(const std::string& filepath);
 	~Shader();
@@ -23,6 +23,8 @@ public:
 	void Unbind() const;
 
 	//Set uniforms
+	void SetUniform1i(const std::string& name, int value);
+	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 
 private:
--- CubeRendering/main.cpp
+++ CubeRendering/main.cpp
@@ -13,6 +13,7 @@
 #include "VertexArray.h"
 #include "VertexBufferLayout.h"
 #include "Shader.h"
+#include "Texture.h"
 
 
 int main(void)
@@ -28,7 +29,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Cube Rendering", NULL, NULL);
+    window = glfwCreateWindow(800, 800, "Cube Rendering", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -43,10 +44,10 @@ int main(void)
         std::cout << "ERROR!" << std::endl;
     {
         float positions[] = {
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-            -0.5f,  0.5f, 
+            -0.5f, -0.5f, 0.0f, 0.0f, //Bottom left
+             0.5f, -0.5f, 1.0f, 0.0f, //Bottom right
+             0.5f,  0.5f, 1.0f, 1.0f, //Top right
+            -0.5f,  0.5f, 0.0f, 1.0f //Top left
         };
 
         unsigned int indices[] = {
@@ -54,19 +55,29 @@ int main(void)
             2, 3, 0
         };
 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+        
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
         
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb, layout);
 
         IndexBuffer ib(indices, 6);
 
+        //
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
 
-        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+        Texture texture("res/textures/Dice.png");
+
+        shader.SetUniform4f("u_BackgroundColor", 1.0f, 1.0f, 1.0f, 0.9f);
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
+
 
         va.Unbind();
         shader.Unbind();
@@ -81,12 +92,14 @@ int main(void)
         {
             /* Render here */
             renderer.Clear();
-
+            
+            /*
             shader.Bind();
             shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
             va.Bind();
             ib.Bind();
+            */
 
             renderer.Draw(va, ib, shader);
 
