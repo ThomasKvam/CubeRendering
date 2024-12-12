2be605faa4c9417bddab08c0f07d5216b0d73c7c
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-12-12 11:23:04 +0100

    Changed the rotation formula to be more consistent using glfwGetTime() and creating a delta time variable

--- CubeRendering/main.cpp
+++ CubeRendering/main.cpp
@@ -27,6 +27,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -41,9 +42,11 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    //Initilizing the GLEW library
     if (glewInit() != GLEW_OK)
         std::cout << "ERROR!" << std::endl;
     {
+        //Defining the front and backface of the cube
         float positions[] = {
             -150.0f, -150.0f, -150.0f, 0.0f, 0.0f, //Bottom left
             150.0f, -150.0f, -150.0f, 1.0f, 0.0f, //Bottom right
@@ -56,6 +59,7 @@ int main(void)
             -150.0f, 150.0f, 150.0f, 0.0f, 1.0f, //Top left
         };
 
+        //Connecting lines between front- and backface points 
         unsigned int indices[] = {
             // Front face
             0, 1, 2,
@@ -82,12 +86,15 @@ int main(void)
             1, 0, 4
         };
 
+        //Enabling blend texture rendering
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
         
+        //Initilizing the VertexArray class
         VertexArray va;
         VertexBuffer vb(positions, 8 * 5 * sizeof(float));
         
+        //Initilizing the VertexBuffer class
         VertexBufferLayout layout;
         layout.Push<float>(3);
         layout.Push<float>(2);
@@ -95,24 +102,21 @@ int main(void)
 
         IndexBuffer ib(indices, 36);
 
-        //Projection
+        //Projection defenition (-x, x, -y, y, -z, z)
         glm::mat4 proj = glm::ortho(-400.0f, 400.0f, -400.0f, 400.0f, -400.0f, 400.0f);
 
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+        //If you want to move the cube:
+        // glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
 
-        glm::mat4 mvp = proj * view; // *model;
-
+        //Binding the shader and pgn to the Cube
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
-
         Texture texture("res/textures/blue.png");
 
-        //Adding a background color to the square
+        //Adding a background color to the cube
         shader.SetUniform4f("u_BackgroundColor", 1.0f, 1.0f, 1.0f, 0.9f);
 
-        shader.SetUniformMat4f("u_MVP", mvp);
-
-        //Adding the png to the square
+        //Adding the png to the cube
         texture.Bind();
         shader.SetUniform1i("u_Texture", 0);
 
@@ -122,39 +126,45 @@ int main(void)
         vb.Unbind();
         ib.Unbind();
 
+        //Initilizing the renderer class
         Renderer renderer;
 
         float rotationX = 0.0f;
         float rotationY = 0.0f;
 
+        //Timing variable
+        double lastTime = glfwGetTime();
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
 
-            /* Calculate rotation */
-            rotationX += 0.05f; // Adjust speed here
-            if (rotationX > 100.0f) // Optional: Reset after a full rotation
-                rotationX -= 0.01f;
-            else if (rotationX < 100.0f) rotationX += 0.01f;
+            //calculate elapsed time
+            double currTime = glfwGetTime();
+            double deltaTime = currTime - lastTime;
+            lastTime = currTime;
+
+            //Update rotation angles
+            const GLfloat rotationSpeed = 40.0f; //degrees per second
+            rotationX += rotationSpeed * deltaTime;
+            rotationY += rotationSpeed * deltaTime;
 
 
-            rotationY += 0.05f; // Adjust speed here
-            if (rotationY > 100.0f) // Optional: Reset after a full rotation
-                rotationY -= 0.01f;
-            else if (rotationY < 100.0f) rotationY += 0.01f;
+            //Rotate around the X and Y axis at origo
+            glm::mat4 modelX = glm::rotate(glm::mat4(1.0f), glm::radians(rotationX), glm::vec3(0.0f, 1.0f, 0.0f));
+            glm::mat4 modelY = glm::rotate(glm::mat4(1.0f), glm::radians(rotationY), glm::vec3(1.0f, 0.0f, 1.0f));
 
-            glm::mat4 modelX = glm::rotate(glm::mat4(0.05f), glm::radians(rotationX), glm::vec3(0.0f, 1.0f, 0.0f));
-            glm::mat4 modelY = glm::rotate(glm::mat4(0.05f), glm::radians(rotationY), glm::vec3(1.0f, 0.0f, 0.0f));
-            glm::mat4 mvp = proj * view * modelX * modelY;
+            //Add the roation to the projection and view
+            glm::mat4 mvp = proj * modelX * modelY;
 
+            //Binding shaders continously because the cube position changes
             shader.Bind();
             shader.SetUniformMat4f("u_MVP", mvp);
-
 
             /* Render here */
             renderer.Clear();
 
+            //Drawing the cube 
             renderer.Draw(va, ib, shader);
 
             /* Swap front and back buffers */
